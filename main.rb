0df7f4ff38284cc9b3cef9de1d41820c9701076d
Julien Desrosiers <juliend2@gmail.com>
2021-02-02 15:59:34 +0000

    Changed some quotes

--- main.rb
+++ main.rb
@@ -18,10 +18,10 @@ day_of_month = Time.now.day
 quotes = [
   "Ce que tu ne sais pas te coûte de l'argent. ce que tu sais te fait gagner de l'argent.",
   "Le succès n'est pas dans le fait dans l'atteinte d'un but, mais dans le fait d'avoir un système en marche vers ta vision/mission.",
-  "Est-ce que tu marche vers ta vision, aujourd'hui ?",
+  "Subdiviser les grosses tâches intimidantes en sous-tâches pour contourner la procrastination.",
+  "Good enough now is better than perfect too late.",
   "_Memento Mori_: Un jour, tu va mourir.",
   "\"Je suis le cep, vous êtes les sarments. Celui qui demeure en moi et en qui je demeure porte beaucoup de fruit, car sans moi vous ne pouvez rien faire.\" -- Jean 15.5",
-  "Is there something unnecessary that I do? If so, I need to stop doing it.",
   "Faire une chose à la fois. sur plusieurs années, est mieux que de faire plusieurs choses en même temps. Apprends à dire non à certaines de tes idées.",
   "\"It's better to have a bad plan than no plan\" -- Peter Thiel",
   "Toujours s'arranger pour être libre financièrement; ça laisse un levier lors de négociations.",
